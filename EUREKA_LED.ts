2257441536407c75dc1950a67a8be8c187f234bc
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-11-10 23:04:56 +0900

    Update EUREKA_LED.ts

--- EUREKA_LED.ts
+++ EUREKA_LED.ts
@@ -1,141 +1,65 @@
-enum vibrater_onoff {
-    ON,
-    OFF,
+enum eureka_IO {
+  Aﾎﾟｰﾄ,
+  Bﾎﾟｰﾄ,
+  Cﾎﾟｰﾄ,
 }
-enum daisyou {
-    大きい,
-    小さい,
+enum eureka_tlp {
+  Aﾎﾟｰﾄ,
+  Bﾎﾟｰﾄ,
 }
-enum select_xyz {
-    X,
-    Y,
-    Z,
-    }
+enum onoff {
+  ON,
+  OFF,
+}
+
+enum LED_onoff {
+    点めつしない,
+    A=1,
+    B=2,
+    C=3,
+}
+enum LED_color {
+    赤,
+    オレンジ,
+    き色,
+    みどり,
+    水色,
+    青,
+    むらさき,
+}
+//% color="#3943c2" block="はじめの一歩　V0.90"
+namespace natumi_blocks {
+
+    //% blockId=eureka_buz_set 
+    //% block=ﾕｰﾚｶIOﾎﾞｯｸｽで音をならす
+    // group="1 初期設定"
+    //% color="#ff3d03" 
+    //% weight=59
+
+    export function eureka_buz_set() {
+    pins.analogSetPitchPin(AnalogPin.P8);
+  }
 
 
-//% color="#3943c2" block="上教大_体育V0.98"
-namespace matubara_blocks {
-
-
-  //% color="#3943ff" weight=50 blockId=axis
-  //% block="|%xyz| 角度の値" group="センサー"
-  export function axis( xyz:select_xyz){
-    switch(xyz){
-        case select_xyz.X:
-        return Math.round(input.acceleration(Dimension.X)/1100*90)+90;
-        case select_xyz.Y:
-        return Math.round(input.acceleration(Dimension.Y)/1100*90);
-        case select_xyz.Z:
-        return Math.round(input.acceleration(Dimension.Z)/1100*90);
+    //% color="#4741f1" weight=54 blockId=eureka_tl_blue block="青信号 点灯|%mode| |%pin|" group="2_信号機ユニット"
+    export function eureka_tl_blue(mode: onoff, pin: eureka_tlp) {
+    switch (pin) {
+      case eureka_tlp.Aﾎﾟｰﾄ:
+        if (mode == onoff.ON) {
+          return pins.digitalWritePin(DigitalPin.P14, 1);
+        } else {
+          return pins.digitalWritePin(DigitalPin.P14, 0);
+        }
+      case eureka_tlp.Bﾎﾟｰﾄ:
+        if (mode == onoff.ON) {
+          return pins.digitalWritePin(DigitalPin.P16, 1);
+        } else {
+          return pins.digitalWritePin(DigitalPin.P16, 0);
+        }
     }
   }
 
 
- //% weight=39 blockId=x_ude_more block="X軸＿腕をふる角度が |%limit| 度より |%kakudo|" group="センサー"
-    export function x_ude_more (limit:number,kakudo:daisyou): boolean {
-    switch(kakudo){
-        case daisyou.大きい:
-        if ((input.acceleration(Dimension.X)/1100*90)+90 > limit )
-            {
-            return true;
-            }
-            else
-            {
-            return false;
-            }
-        case daisyou.小さい :
-            if ((input.acceleration(Dimension.X)/1100*90)+90 < limit ){
-            return true;
-            }
-            else
-            {
-            return false;
-            }
-        }
-    }
-
- 
- //% weight=37 blockId=y_ude_more block="ｙ軸＿腕をふる角度が |%limit| 度より |%kakudo|" group="センサー"
-  export function y_ude_more(limit: number,kakudo:daisyou): boolean {
-     switch(kakudo){
-        case daisyou.大きい:
-        if ((input.acceleration(Dimension.Y)/1100*90) > limit ){
-            return true;
-            }else{
-            return false;
-        }
- 
-        case daisyou.小さい :
-        if ((input.acceleration(Dimension.Y)/1100*90) < limit ){
-            return true;
-            }else{
-            return false;
-        }
-    }   
-  }
- 
- //% weight=35 blockId=z_ude_more block="ｚ軸＿腕をふる角度が |%limit| 度より |%kakudo|" group="センサー"
-  export function z_ude_more(limit: number,kakudo:daisyou): boolean {
-     switch(kakudo){
-        case daisyou.大きい:
-        if ((input.acceleration(Dimension.Z)/1100*90) > limit ){
-            return true;
-            }else{
-            return false;
-        }
-        
-        case daisyou.小さい :
-        if ((input.acceleration(Dimension.Z)/1100*90) < limit ){
-            return true;
-            }else{
-            return false;
-        }
-    }
-    }   
-
- //% color="#ffa800" weight=23 blockId=vib_kanketu block="振動する（音なし）" group="機能"
-  export function vib_kanketu() {
-    let i=0;
-    for (let i=1 ; i<3 ;i++)　{
-            basic.pause(100); 
-            pins.digitalWritePin(DigitalPin.P1, 1); 
-            basic.pause(400);  
-            pins.digitalWritePin(DigitalPin.P1, 0);
-            basic.pause(400); 
-        }
-    }
-
- //% color="#ffa800" weight=23 blockId=vib_kanketu_oto block="振動する（音付き）" group="機能"
-  export function vib_kanketu_oto() {
-    let i=0;
-    for (let i=1 ; i<3 ;i++)　{
-            basic.pause(100); 
-            pins.digitalWritePin(DigitalPin.P1, 1); 
-            music.ringTone(698)
-            basic.pause(400);  
-            pins.digitalWritePin(DigitalPin.P1, 0);
-            music.rest(music.beat(BeatFraction.Sixteenth))           
-            basic.pause(400); 
-        }
-    }
-
-
-
- //% color="#ffa800" weight=12 blockId=vib block="振動 |%mode|" group="機能"
-  export function vib(mode: vibrater_onoff) {
-            if (mode == vibrater_onoff.ON ) {
-            return pins.digitalWritePin(DigitalPin.P1, 1);
-            } else {
-            return pins.digitalWritePin(DigitalPin.P1, 0);
-        }
-    }   
-
-  
-    
- //% color="#ff3d03" weight=11 blockId=oto block="音を鳴らす" group="機能"
-  export function oto() {
-        music.playTone(659, music.beat(BeatFraction.Eighth))
-  }
 
   //% color="#1E90FF" weight=10 block="待ち時間（秒）|%second|" group="機能"
   //% second.min=0 second.max=10
